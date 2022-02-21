94373cbf4077d0827d9fe230c0f05f000daadfb1
Moritz Wetzel <moritz_w@aol.de>
2022-02-21 15:45:56 +0100

    refactored code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pm4py
+from pm4py.objects.log.importer.xes import importer as xes_importer
+
 a = 3
+log = xes_importer.apply('data/BPI_Challenge_2017.xes')
 
 if a == 3:
-    from pm4py.objects.log.importer.xes import importer as xes_importer
-    log_data = xes_importer.apply('data/BPI_Challenge_2017.xes')
-    for trace in log_data:
+    for trace in log:
         print(trace)
         for event in trace:
             eventId = event["EventID"]
@@ -12,23 +14,7 @@ if a == 3:
             action = event["Action"]
             name = trace.__dict__["_attributes"]["concept:name"]
 
-            #log1 = log[0]
-            #log2 = log[1]
-            #log3 = log[2]
-            #log4 = log[3]
-            #log5 = log[4]
-
-            #print(f"Log 1: {log1}")
-            #print(f"Log 2: {log2}")
-            #print(f"Log 3: {log3}")
-            #print(f"Log 4: {log4}")
-            #print(f"Log 5: {log5}")
-
 if a == 2:
-    import pm4py
-    from pm4py.objects.log.importer.xes import importer as xes_importer
-
-    log = xes_importer.apply('data/BPI_Challenge_2017.xes')
 
     log1 = log[0]
     log2 = log[1]
