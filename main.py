8cdb222b5d034917ddabb6f01d9af593f59096c2
Moritz Wetzel <moritz_w@aol.de>
2022-02-21 15:38:28 +0100

    commit name is available in each trace

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from pm4py.objects.log.importer.xes import importer as xes_importer
 
-log = xes_importer.apply('data/BPI_Challenge_2017.xes')
+log_data = xes_importer.apply('data/BPI_Challenge_2017.xes')
 
-log1 = log[0]
-log2 = log[1]
-log3 = log[2]
-log4 = log[3]
-log5 = log[4]
+for trace in log_data:
+    print(trace)
+    for event in trace:
+        eventId = event["EventID"]
+        user = event["org:resource"]
+        time = event["time:timestamp"]
+        action = event["Action"]
+        name = trace.__dict__["_attributes"]["concept:name"]
 
-print(f"Log 1: {log1}")
-print(f"Log 2: {log2}")
-print(f"Log 3: {log3}")
-print(f"Log 4: {log4}")
-print(f"Log 5: {log5}")
+#log1 = log[0]
+#log2 = log[1]
+#log3 = log[2]
+#log4 = log[3]
+#log5 = log[4]
+
+#print(f"Log 1: {log1}")
+#print(f"Log 2: {log2}")
+#print(f"Log 3: {log3}")
+#print(f"Log 4: {log4}")
+#print(f"Log 5: {log5}")
